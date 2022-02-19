3732317f8ee56051553e6b9d767c7eff8b8b91e8
Jed <jedtony.jt@gmail.com>
2022-02-19 19:41:43 +0100

    Add migration for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,26 @@ return new class extends Migration
      */
     public function up()
     {
+        if(!Schema::hasTable('users'))
+        {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('uuid')->comment('UUID to allow easy migration between envs without breaking FK in the logic');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable()->comment('nullable, UUID of the image stored into the files table ');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->boolean('is_marketing')->default(false)->comment('Enable marketing preferences: default (0)');
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
         });
+    }
     }
 
     /**
