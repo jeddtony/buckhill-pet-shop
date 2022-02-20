c202068dfb47543968f645a177c0edace2ca23be
Jed <jedtony.jt@gmail.com>
2022-02-20 12:13:41 +0100

    add response formats

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -10,4 +12,26 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function formatSuccessResponse(string $message, $data)
+    {
+        return response()->json(
+            ['status' => true, 'message' => $message, 'data' => $data], 200);
+    }
+
+    public function formatCreatedResponse(string $message, $data)
+    {
+        return response()->json(
+            ['status' => true, 'message' => $message, 'data' => $data], 201);
+    }
+
+    public function formatInputErrorResponse(string $message)
+    {
+        return response()->json(['status' => false, 'message' => $message], 422);
+    }
+
+    public function notFoundResponse(string $message)
+    {
+        return response()->json(['status' => false, 'message' => $message], 404);
+    }
 }
