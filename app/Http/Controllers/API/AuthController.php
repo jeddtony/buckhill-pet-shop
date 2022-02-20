f7cdea744eaba90bd16503e0a58c6e66171d043e
Jed <jedtony.jt@gmail.com>
2022-02-20 12:15:27 +0100

    Add authentication controller

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'first_name' => 'required|max:55',
+            'last_name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required',
+            'address' => 'required|max:255',
+            'phone_number' => 'required|max:255',
+        ]);
+
+        
+        $validatedData['password'] = bcrypt($request->password);
+        $validatedData['uuid'] = Str::uuid();
+
+        $user = User::create($validatedData);
+
+        
+        $access_token = $user->createToken('authToken')->accessToken;
+        
+        
+        $data = [
+            'user' => $user,
+            'access_token' => $access_token
+        ];
+        return $this->formatCreatedResponse('Registration successful', $data);
+    }
+
+    public function login(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required',
+            'password' => 'required'
+        ]);
+
+
+        if($validator->fails()) {
+            $result = $this->formatInputErrorResponse($validator->errors());
+            return $result;
+        }
+
+        $loginData = ['email' => $request->email, 'password' => $request->password];
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        $data = [
+            'user' => auth()->user(),
+            'access_token' => $accessToken
+        ];
+        
+        return $this->formatSuccessResponse('Login successful', $data);
+
+    }
+}
