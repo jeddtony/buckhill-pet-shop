55456bf75d95bb8d7bde6af8c76c011db1745aba
Jed <jedtony.jt@gmail.com>
2022-02-24 23:31:42 +0100

    Add payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['uuid', 'details', 'type'];
 }
