02be858df4f2669584097a7249241d8af4061063
Jed <jedtony.jt@gmail.com>
2022-02-28 00:31:30 +0100

    Bug fix on hashing password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class DatabaseSeeder extends Seeder
         // Create admin user
         User::factory()->create([
             'email' => 'admin@buckhill.co.uk',
-            'password' => 'admin',
+            'password' => Hash::make('admin'),
             'is_admin' => true
         ]);
 
