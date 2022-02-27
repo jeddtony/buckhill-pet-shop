be75aba29101a5c65d2874fa8d72c808acb3a514
Jed <jedtony.jt@gmail.com>
2022-02-28 00:22:31 +0100

    Add user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -18,10 +19,17 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'uuid' => Str::uuid(),
+            'first_name' => $this->faker->name(),
+            'last_name' => $this->faker->name(),
+            'is_admin' => $this->faker->boolean(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('userpassword'), // password
+            'avatar' => $this->faker->imageUrl(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'is_marketing' => $this->faker->boolean(),
             'remember_token' => Str::random(10),
         ];
     }
