aa82cd09ed2fcbb380fe0f7f14380100bf9518f1
Jed <jedtony.jt@gmail.com>
2022-02-28 00:22:55 +0100

    Add database seeder for users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Create admin user
+        User::factory()->create([
+            'email' => 'admin@buckhill.co.uk',
+            'password' => 'admin',
+            'is_admin' => true
+        ]);
+
+        // Create 10 non-admin users
+        User::factory()->count(10)->create();
+
         // \App\Models\User::factory(10)->create();
     }
 }
